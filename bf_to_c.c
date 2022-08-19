87e78e6143516861967bd54ae823bbabc8047236
Sezgin Tunay <110986949+vladimirdelvis@users.noreply.github.com>
2022-08-19 13:37:33 +0300

    Update v2
    Update: Now all your arguments and number of your arguments you give to your application will be loaded into the brainf*ck memory.

--- bf_to_c.c
+++ bf_to_c.c
@@ -36,15 +36,26 @@ static char* getchars(){
 
 int main(int argc, char** argv){
 
-    int max_mem = 0;
+    int max_mem = 0,arg_index = 0;
     puts("MAX MEMORY : ");
     scanf("%u",&max_mem);
+    getchar();
+    puts("ARGUMENT LOCATION (INDEX) : ");
+    scanf("%u",&arg_index);
     getchar();
     FILE *fl = fopen("program.c","w");
     char* ptr = getchars();
     int index = 0;
     char test_val = 0;
-    fprintf(fl,"%s%d%s","int main(){\nunsigned char *pointer = calloc(",max_mem,",1);\nunsigned char* ptr2 = pointer;\n");
+    fprintf(fl,"%s%d%s%d%s","#include <stdio.h>\n#include <string.h>\n\n"
+    "int main(int argc, char **argv){\nunsigned char *pointer = calloc(",max_mem,",1);\n"
+    "unsigned char* ptr2 = pointer;\n"
+    "unsigned long endstr_long = strlen(argv[argc - 1]) + 1;\n"
+    "unsigned long length = ((unsigned long)argv[argc - 1] + endstr_long) - (unsigned long)*argv;\n"
+    "char *location = ptr2 + ",arg_index,
+    ";\n*(int*)location = argc;"
+    "\nlocation += sizeof(int);\n"
+    "memcpy(location,argv[0],length);\n");
     fflush(fl);
     for(;;){
         if(ptr[index] == '\0'){
